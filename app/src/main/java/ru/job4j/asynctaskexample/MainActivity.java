46bf97980355c420d766ab0e0484272f3bd0d424
DmK78 <dmk78@inbox.ru>
2019-09-05 12:50:23 +0500

    03 - AsyncTask[#162815] при повороте экрана счетчик сохраняется, сама asynctask останавливается. Затем с возовновленным счетчиком продолжается

--- app/src/main/java/ru/job4j/asynctaskexample/MainActivity.java
+++ app/src/main/java/ru/job4j/asynctaskexample/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
@@ -15,7 +16,9 @@ public class MainActivity extends AppCompatActivity {
 
     ProgressBar progressBar;
     Button button;
-
+    SampleAsyncTask sampleAsyncTask;
+    private int initialCount=30;
+    int count=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,26 +26,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         progressBar = findViewById(R.id.progressBar);
         button = findViewById(R.id.buttonStart);
+        if (savedInstanceState != null) {
+            count = savedInstanceState.getInt("count", -1);
+            if (count > 0) {
+                sampleAsyncTask=new SampleAsyncTask(this,count);
+                sampleAsyncTask.execute(initialCount);
+            }
+        }
     }
 
     public void startAsyncTask(View view) {
         progressBar.setVisibility(View.VISIBLE);
-        SampleAsyncTask sampleAsyncTask = new SampleAsyncTask(this);
-        sampleAsyncTask.execute(30);
+        if (sampleAsyncTask == null) {
+            sampleAsyncTask = new SampleAsyncTask(this,0);
+            sampleAsyncTask.execute(initialCount);
+        }
     }
 
     private static class SampleAsyncTask extends AsyncTask<Integer, Integer, String> {
         private WeakReference<MainActivity> activityWeakReference;
+        public int count;
 
-        SampleAsyncTask(MainActivity activity) {
-            activityWeakReference = new WeakReference<>(activity);
+        SampleAsyncTask(MainActivity act, int count) {
+            activityWeakReference = new WeakReference<>(act);
+            this.count=count;
         }
 
         @Override
         protected String doInBackground(Integer... integers) {
-            int count = 0;
             while (count < integers[0]) {
                 publishProgress((count * 100) / integers[0]);
+                Log.i("asyncTask","asyncTask hashcode: "+ this.hashCode()+", count "+count);
+                if(isCancelled())
+                    break;
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
@@ -54,12 +70,14 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
+        protected void onCancelled(String s) {
+            super.onCancelled(s);
+        }
+
+        @Override
         protected void onPreExecute() {
             super.onPreExecute();
             MainActivity activity = activityWeakReference.get();
-            if (activity == null || activity.isFinishing()) {
-                return;
-            }
             activity.progressBar.setVisibility(View.VISIBLE);
         }
 
@@ -67,9 +85,6 @@ public class MainActivity extends AppCompatActivity {
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
             MainActivity activity = activityWeakReference.get();
-            if (activity == null || activity.isFinishing()) {
-                return;
-            }
             Toast.makeText(activity, s, Toast.LENGTH_SHORT).show();
             activity.progressBar.setProgress(0);
             activity.progressBar.setVisibility(View.INVISIBLE);
@@ -79,13 +94,14 @@ public class MainActivity extends AppCompatActivity {
         protected void onProgressUpdate(Integer... values) {
             super.onProgressUpdate(values);
             MainActivity activity = activityWeakReference.get();
-            if (activity == null || activity.isFinishing()) {
-                return;
-            }
             activity.progressBar.setProgress(values[0]);
-
         }
     }
 
-
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("count", sampleAsyncTask.count);
+        sampleAsyncTask.cancel(true);
+    }
 }
